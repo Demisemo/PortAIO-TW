478e82f13f6a280a9898da3a2e13d970412e70d8
YukinoMo0411 <Demisemo@gmail.com>
2017-02-04 08:36:07 +0800

    .

--- LanguageTranslator/Program.cs
+++ LanguageTranslator/Program.cs
@@ -20,7 +20,7 @@ namespace LanguageTranslator
     internal static class Program
     {
         private const string VersionUrl = "https://raw.githubusercontent.com/Demisemo/PortAIO-TW/blob/master/LanguageTranslator/Properties/AssemblyInfo.cs";
-        private const string JsonUrl = "https://raw.githubusercontent.com/Demisemo/PortAIO-TW/blob/master/LanguageTranslator/Translations.json";
+        private const string JsonUrl = "https://raw.githubusercontent.com/finndev/iCreative-Mirror/master/LanguageTranslator/LanguageTranslator/Translations.json";
         private const string VersionRegex = @"\[assembly\: AssemblyVersion\(""(\d+\.\d+\.\d+\.\d+)""\)\]";
         private static string _jsonPath;
         private static string _programDirectory;
